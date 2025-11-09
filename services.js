2da84139462ed73f725c5358c05e80271b5606e5
Abderrazak <aamaichabderrazak2004@gmail.com>
2025-11-09 16:26:52 +0100

    J'ai ajouter les informations dans le fichier JSON

--- services.js
+++ services.js
@@ -1,8 +1,13 @@
+// Mode gestion
 let paragraph = document.getElementById("paragraph");
 let toggleSwitch = document.getElementById("toggleSwitch");
 let btn_AjouterService = document.getElementById("btn_AjouterService");
-let supprimer_btn = document.getElementById('supprimer_btn');
-let modifier_btn = document.getElementById('modifier_btn');
+let supprimer_btn = document.getElementById("supprimer_btn");
+let modifier_btn = document.getElementById("modifier_btn");
+let return_btn = document.getElementById("return_btn");
+let btn_annuler = document.getElementById("btn_annuler");
+
+
 function ModeFreelance() {
   if (toggleSwitch.checked) {
     paragraph.style.display = "block";
@@ -15,6 +20,31 @@ function ModeFreelance() {
     supprimer_btn.style.display = "none";
     modifier_btn.style.display = "none";
   }
+  // Ajouter un service
+  if (btn_AjouterService) {
+  btn_AjouterService.addEventListener("click", function () {
+    window.location.href = "ajouter_un_service.html";
+  });
 }
-toggleSwitch.addEventListener('change', ModeFreelance);
+
+
+if (btn_annuler) {
+  btn_annuler.addEventListener("click", function () {
+    window.location.href = "services.html";
+  });
+}
+
+if (modifier_btn){
+  modifier_btn.addEventListener("click", function () {
+    window.location.href = "modifier_service.html";
+  });
+}
+}
+toggleSwitch.addEventListener("change", ModeFreelance);
 ModeFreelance();
+
+
+
+  return_btn.addEventListener("click", function () {
+    window.location.href = "services.html";
+  });
