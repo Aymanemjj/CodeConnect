37411bb28d3d16d7bfb65d5a9c76f5558d06b77d
Abderrazak <aamaichabderrazak2004@gmail.com>
2025-11-12 14:22:52 +0100

    J'ai ajouter des fonctionnalités

--- services.js
+++ services.js
@@ -2,49 +2,53 @@
 let paragraph = document.getElementById("paragraph");
 let toggleSwitch = document.getElementById("toggleSwitch");
 let btn_AjouterService = document.getElementById("btn_AjouterService");
-let supprimer_btn = document.getElementById("supprimer_btn");
-let modifier_btn = document.getElementById("modifier_btn");
+let supprimer_btn = document.querySelectorAll(".supprimer_btn");
+let modifier_btn = document.querySelectorAll(".modifier_btn");
 let Modifier_btn = document.getElementById("Modifier_btn");
 let return_btn = document.getElementById("return_btn");
 let btn_annuler = document.querySelectorAll("btn_annuler");
 let categories = document.querySelectorAll(".categorie");
 
 function ModeFreelance() {
-  
   if (toggleSwitch.checked) {
     paragraph.style.display = "block";
     btn_AjouterService.style.display = "block";
-    supprimer_btn.style.display = "block";
-    modifier_btn.style.display = "block";
-    
+    console.log("enter");
+
+    modifier_btn.forEach((element) => {
+      console.log(element);
+      element.classList.replace("hidden", "block");
+    });
+    supprimer_btn.forEach((element) => {
+      element.classList.replace("hidden", "block");
+    });
   } else {
     paragraph.style.display = "none";
     btn_AjouterService.style.display = "none";
-    supprimer_btn.style.display = "none";
-    modifier_btn.style.display = "none";
+    supprimer_btn.forEach((element) => {
+      element.classList.replace("block", "hidden");
+    });
+    modifier_btn.forEach((element) => {
+      element.classList.replace("block", "hidden");
+    });
   }
 }
 toggleSwitch.addEventListener("change", ModeFreelance);
 ModeFreelance();
 
-function navigation(){
+function navigation() {
   btn_AjouterService.addEventListener("click", function () {
-  window.location.href = "ajouter_un_service.html";
-
-});
-  btn_annuler.addEventListener("click",function(){
-  window.open("services.html");
-})
+    window.location.href = "ajouter_un_service.html";
+  });
+  btn_annuler.addEventListener("click", function () {
+    window.open("services.html");
+  });
 }
 navigation();
 
-// trier par categorie 
+// trier par categorie
 
-function trierParCategorie(){
-  let trierCategorie = document.getElementById('trier_Categorie');
-  trierCategorie.addEventListener('change', function(){
-    
-
-  })
-
+function trierParCategorie() {
+  let trierCategorie = document.getElementById("trier_Categorie");
+  trierCategorie.addEventListener("change", function () {});
 }
