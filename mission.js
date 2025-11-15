94a8bb866cf1aeb1a26b63fda6b71424270aed7f
mohamed <medolgholb@gmail.com>
2025-11-15 18:07:56 +0100

    ajouter  regex sur formilaire de missions

--- mission.js
+++ mission.js
@@ -21,8 +21,8 @@ async function missionList() {
             }
         }
 
-        document.getElementById(`mission-prix${num}`).textContent = mission.missionPrix[0]+"$ - "+mission.missionPrix[1]+"$";
-        document.getElementById(`mission-dure${num}`).textContent =mission.missionDure[0]+" - " +mission.missionDure[1]+" monts";
+        document.getElementById(`mission-prix${num}`).textContent = mission.missionPrix[0] + "$ - " + mission.missionPrix[1] + "$";
+        document.getElementById(`mission-dure${num}`).textContent = mission.missionDure[0] + " - " + mission.missionDure[1] + " monts";
         document.getElementById(`mission-contrat${num}`).textContent = mission.missionContrat;
     });
 
@@ -30,3 +30,94 @@ async function missionList() {
 }
 
 missionList();
+let count = 1;
+// ::::::::::::::::::::::::::::med-sajouet
+document.getElementById("ajoutertechno").addEventListener("click", function () {
+
+    let technologieajt = document.getElementById("technologie-ajt");
+    if (count < 5) {
+        count++;
+        document.getElementById("placevide").innerHTML += `<h3 class="border w-fit rounded-sm bg-slate-300 p-1 m-1">${technologieajt.value}</h3>`;
+        technologieajt.value = "";
+    }
+})
+//   :::::::::::::::::::::::::::::::::::::::::
+let form = document.getElementById("form");
+
+form.addEventListener("submit", validationformil);
+
+function validationformil(e) {
+   let tabmissionn=[];
+    e.preventDefault();
+    const titre = document.getElementById("titre-mission-ajt").value.trim();
+    const clientenreprs = document.getElementById("client-entreprise-ajt").value.trim();
+    const description = document.getElementById("description-ajt").value.trim();
+    const duree = document.getElementById("duree-ajt");
+    const budget = document.getElementById("budget-ajt");
+
+
+    if (budget.value == '') {
+
+        budget.style.borderColor = "red";
+        return;
+    } else {
+
+        budget.style.borderColor = "green";
+        
+
+    };
+    const fulname = /^[a-zA-Z]{1,30}$/;
+    if (titre == '') {
+
+        document.getElementById("titre-mission-ajt").style.borderColor = "red";
+        return;
+    } else {
+        if (!fulname.test(titre)) {
+            document.getElementById("titre-mission-ajt").style.borderColor = "red";
+
+            return;
+        } else {
+            document.getElementById("titre-mission-ajt").style.borderColor = "green";
+
+        }
+    };
+    if (clientenreprs == "") {
+        document.getElementById("client-entreprise-ajt").style.borderColor = "red";
+        return;
+    } else {
+        if (!fulname.test(clientenreprs)) {
+            document.getElementById("client-entreprise-ajt").style.borderColor = "red";
+
+            return;
+        }
+        else {
+            document.getElementById("client-entreprise-ajt").style.borderColor = "green";
+
+        }
+    };
+    if (description == "") {
+        document.getElementById("description-ajt").style.borderColor = "red";
+        return;
+    } else {
+        document.getElementById("description-ajt").style.borderColor = "green";
+
+    };
+    if (duree == "") {
+        document.getElementById("titre-mission-ajt").style.borderColor = "red";
+        return;
+    } else {
+        if (duree < 15 || duree > 100) {
+            document.getElementById("duree-ajt").style.borderColor = "red";
+
+            return;
+        } else {
+            document.getElementById("duree-ajt").style.borderColor = "green";
+
+        }
+    };
+    setitemsdanslocalstorege();
+
+}
+function setitemsdanslocalstorege() {
+
+}
